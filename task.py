622c3375297a6566e21464094fceba224aaf9963
drewripa <ripa.andrew@gmail.com>
2018-01-18 11:31:09 +0200

    show ip addr

--- task.py
+++ task.py
@@ -1,7 +1,18 @@
 import boto3
+import socket
+
+servernames = ['one.drewripa.ga', 'two.drewripa.ga', 'three.drewripa.ga']
+for servername in servernames:
+    print(socket.gethostbyname(servername))
 
 ec2 = boto3.resource('ec2')
 
-instances = ec2.instances
+instances = ec2.instances.filter(
+    Filters=[
+        {
+            'Name': 'instance-state-name',
+            'Values': ['running']
+        }
+    ])
 for instance in instances:
-    print(instance.id, instance.instance_type, instance.public_ip_address, instance.state)
+    print(instance.id, instance.instance_type, instance.public_ip_address)
