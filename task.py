c872d0069ea90f6ecefc5d064c09e50e3f3064e2
drewripa <ripa.andrew@gmail.com>
2018-01-18 13:18:01 +0200

    Complex servers info added

--- task.py
+++ task.py
@@ -1,20 +1,28 @@
 import boto3
 import socket
 
-servernames = ['one.drewripa.ga', 'two.drewripa.ga', 'three.drewripa.ga']
-servernamesIP = []
-for servername in servernames:
-    servernamesIP.append(socket.gethostbyname(servername))
-print(servernamesIP)
+#Step 1:
+#Resolve IP addresses for our DNS names
+serverNames = ['one.drewripa.ga', 'two.drewripa.ga', 'three.drewripa.ga']
+serverIPs = []
+for serverName in serverNames:
+    serverIPs.append(socket.gethostbyname(serverName))
+#TODO: Remove line
+print(serverIPs)
 
+#Step 2:
+#Get access to our EC2 instances via awscli (using boto3 lib)
 ec2 = boto3.resource('ec2')
+complexInstanceInfo=[]
 
 instances = ec2.instances.filter(
     Filters=[
         {
             'Name': 'ip-address',
-            'Values': servernamesIP
+            'Values': serverIPs
         }
     ])
 for instance in instances:
-    print(instance.id, instance.instance_type, instance.public_ip_address)
+    complexInstanceInfo.append(['instance.id', 'instance.public_ip_address', serverNames[instance.public_ip_address]])
+#TODO: Remove line
+print(complexInstanceInfo)
